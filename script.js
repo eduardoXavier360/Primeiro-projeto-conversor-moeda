f90003a1dd203cf2f44ae8c191889bd816c86371
eduardoXavier360 <eduardokungfu01@hotmail.com>
2025-02-15 19:36:33 -0300

    adicionei bitcoin

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ function convertValues() {
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // moeda real
     const currencyValueConverted = document.querySelector(".currency-value") // Outras moedas
 
-    const dolarToday = 5.2
+    const dolarToday = 5.6 
     const euroToday = 6.2
+    const bitcoinToday = 5.54
 
     if (currencySelect.value == "Dolar") {
 
@@ -25,6 +26,14 @@ function convertValues() {
         }).format(inputCurrencyValue / euroToday)
     }
 
+
+    if (currencySelect.value == "Bitcoin") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("BTC", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputCurrencyValue / bitcoinToday)
+    }
+
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
@@ -37,19 +46,25 @@ function changeCurrency() {
     const currencyName = document.getElementById("currency-name")
     const currencyImage = document.querySelector(".currency-img")
 
+
     if (currencySelect.value == "Dolar") {
         currencyName.innerHTML = "Dólar-Americano"
         currencyImage.src = "/assets/USA.png"
     }
-        if (currencySelect.value == "Euro") {
-            currencyName.innerHTML = "Euro"
-            currencyImage.src ="./assets/euro.png"
-        }
 
-        convertValues()
-
+    if (currencySelect.value == "Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.png"
     }
 
+    if (currencySelect.value == "Bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+      currencyImage.src ="./assets/bitcoin 1.png"
+    }
+    convertValues()
 
-    currencySelect.addEventListener("change", changeCurrency)
-    convertButton.addEventListener("click", convertValues) 
+}
+
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues) 
